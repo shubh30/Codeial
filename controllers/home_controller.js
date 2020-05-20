8baa54092908c33b99826413934d8537964e587e
Shubham <shubhamnagpal30@gmail.com>
2020-05-20 18:37:21 +0530

    Add a contrller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req, res){
+    return res.end('<h1>Express is up for codeial</h1>');
+}
+
+// module.exports.bye = function(req, res) {
+//     return res.end('<h1>Bye Bye....</h1>');
+// }
