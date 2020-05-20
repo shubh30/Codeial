b8397d04de75d70bb4b0743e41b43ab8bafc1079
Shubham <shubhamnagpal30@gmail.com>
2020-05-20 19:26:21 +0530

    Setup View Engine

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+// Setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running the server: ${err}`);
