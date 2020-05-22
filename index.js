5c17e9204d0bf65e5da2605449a3da1a8ec115a0
Shubham <shubhamnagpal30@gmail.com>
2020-05-22 21:40:54 +0530

    Added static files access for layout and pages

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+// Extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // use express router
 app.use('/', require('./routes'));
