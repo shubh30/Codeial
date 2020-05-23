24a31c0e5a11f829044eeb5a27c28128ad4eae87
Shubham <shubhamnagpal30@gmail.com>
2020-05-23 21:40:24 +0530

    Created user using sign up and added to schema

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('profile', {
         title: "profile"
@@ -20,7 +22,23 @@ module.exports.signIn = function(req, res) {
 
 // Get the sign up data
 module.exports.create = function(req, res) {
-    // TODO Later
+    if(req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user) {
+        if(err){console.log('Error in finding user in sign up'); return}
+
+        if(!user) {
+            User.create(req.body, function(err, user) {
+                if(err){console.log('Error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    })
 }
 
 module.exports.createSession = function(req, res) {
