ed809a766ef10f7747ffcd77d63bc5bea7847214
Shubham <shubhamnagpal30@gmail.com>
2020-05-27 19:29:21 +0530

    Added comments to DB and display them on home

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include array of ids of all comments in the post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 }, {
     timestamps: true
 });
