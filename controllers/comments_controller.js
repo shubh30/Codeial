025016dacb0d5c8bd71647fce353a62707a6d16c
Shubham <shubhamnagpal30@gmail.com>
2020-06-01 13:42:20 +0530

    Converted code to Async - Await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,23 +1,26 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.findById(req.body.post);
+
         if(post) {
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
-                // handle error
-
-                post.comments.push(comment);
-                post.save();
-
-                res.redirect('back');
             });
+
+            post.comments.push(comment);
+            post.save();
+
+            res.redirect('back');
         }
-    });
+    } catch(err) {
+        console.log('Error', err);
+        return;
+    }
 }
 
 module.exports.destroy = function(req, res){
