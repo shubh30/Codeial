5b03abb71f945a316eb519f9e3d86e4b1a6b9a8d
Shubham <shubhamnagpal30@gmail.com>
2020-06-02 20:37:07 +0530

    Added delete function to delete the post dynamically

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 }, error: function(error) {
                     console.log(error.responseText);
                 }
@@ -26,7 +27,7 @@
                     <p>
                         
                         <small>
-                            <a class="delete-post-button" href="/posts/destroy/${ post.id }">X</a>
+                            <a class="delete-post-button" href="/posts/destroy/${ post._id }">X</a>
                         </small>
                         
                         ${ post.content }
@@ -50,6 +51,23 @@
                         </div>
                     </div>
                 </li>`)
+    }
+
+    // Method to delete a post from DOM
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault;
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        })
     }
 
     createPost();
