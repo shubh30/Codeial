61fcafce85c4f6925792d965921927dce1a5c4d4
Shubham <shubhamnagpal30@gmail.com>
2020-06-03 17:45:28 +0530

    Saved Avatars in directory from params using Multer

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -14,6 +14,15 @@
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost));
+
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post published!",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
                 }, error: function(error) {
                     console.log(error.responseText);
                 }
@@ -63,6 +72,15 @@
                 url: $(deleteLink).prop('href'),
                 success: function(data){
                     $(`#post-${data.data.post_id}`).remove();
+
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post Deleted",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
                 }, error: function(error){
                     console.log(error.responseText);
                 }
