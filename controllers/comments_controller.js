25e9302964e4ae0399a13b615970f63f62116139
Shubham <shubhamnagpal30@gmail.com>
2020-06-09 13:45:35 +0530

    Set up Kue and implemented delayed job

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req, res){
     try{
@@ -17,7 +19,15 @@ module.exports.create = async function(req, res){
             post.save();
 
             comment = await comment.populate('user', 'name email').execPopulate();
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+            let job = queue.create('emails', comment).save(function(err){
+                if(err) {
+                    console.log('Error in creating a queue', err);
+                    return;
+                }
+
+                console.log('Job enqueued', job.id)
+            });
             if(req.xhr){
                 
 
