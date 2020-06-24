aa90e50f14064c6d6f3b3e8c3d5228d525c7aaf3
Shubham <shubhamnagpal30@gmail.com>
2020-06-24 11:50:56 +0530

    Add toggle like

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,6 +3,7 @@ const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
 const queue = require('../config/kue');
 const commentEmailWorker = require('../workers/comment_email_worker');
+const Like = require('../models/like');
 
 module.exports.create = async function(req, res){
     try{
@@ -49,17 +50,44 @@ module.exports.create = async function(req, res){
     }
 }
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id, function(err, comment){
-        if(comment.user == req.user.id){
+module.exports.destroy = async function(req, res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
+
             comment.remove();
 
-            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
-                return res.redirect('back');
-            })
-        } else {
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // CHANGE :: destroy the associated likes for this comment
+            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
+
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    })
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
+    
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const Like = require('../models/like');
 
 module.exports.create = async function(req, res) {
     try{
@@ -9,18 +10,24 @@ module.exports.create = async function(req, res) {
         });
 
         if(req.xhr){
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
+
             return res.status(200).json({
                 data: {
                     post: post
                 },
-                message: "Post Created!"
+                message: "Post created!"
             });
         }
     
         req.flash('success', 'Post Published');
         return res.redirect('back');
+
     } catch(err) {
         req.flash('error', err);
+        // added this to view the error on console as well
+        console.log(err);
         return res.redirect('back');
     }
 }
@@ -31,6 +38,11 @@ module.exports.destroy = async function(req, res) {
 
         // .id means converting the object id into string
         if(post.user == req.user.id){
+
+            // CHANGE :: delete the associated likes for the post and all its comments' likes too
+            await Like.deleteMany({likeable: post, onModel: 'Post'});
+            await Like.deleteMany({_id: {$in: post.comments}});
+
             post.remove();
             await Comment.deleteMany({post: req.params.id});
 
