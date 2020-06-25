901a51e2db3dc8fd4b377815548b039f2ee5d49a
Shubham <shubhamnagpal30@gmail.com>
2020-06-25 13:29:48 +0530

    Add chatbox functionality successfully

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -23,6 +23,43 @@ class ChatEngine{
       self.socket.on('user_joined', function(data){
         console.log('A user joined', data);
       })
+    });
+
+    $('#send-message').click(function(){
+      let msg = $('#chat-message-input').val();
+
+      if(msg != '') {
+
+        self.socket.emit('send_message', {
+          message: msg,
+          user_email: self.userEmail,
+          chatroom: 'codeial'
+        });
+      }
+    });
+
+    self.socket.on('recieve_message', function(data) {
+      console.log('Message recieved', data.message);
+
+      let newMessage = $('<li>');
+
+      let messageType = 'other-message';
+
+      if(data.user_email == self.userEmail){
+        messageType = 'self-message';
+      }
+
+      newMessage.append($('<span>', {
+        'html': data.message
+      }));
+
+      newMessage.append($('<sub>', {
+        'html': data.user_email
+      }));
+
+      newMessage.addClass(messageType);
+
+      $('#chat-messages-list').append(newMessage);
     })
   }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -14,6 +14,10 @@ module.exports.chatSockets = function(socketServer) {
       socket.join(data.chatroom);
 
       io.in(data.chatroom).emit('user_joined', data);
-    })
+    });
+
+    socket.on('send_message', function(data){
+      io.in(data.chatroom).emit('recieve_message', data);
+    });
   });
 }
